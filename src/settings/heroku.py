43c37a27ab8882bc4ce2c9bd2b5c5d7a1d89e3a9
kobtsev-m <kobtsev.x@gmail.com>
2021-01-31 17:42:20 +0700

    Caches settings

--- src/settings/heroku.py
+++ src/settings/heroku.py
@@ -11,3 +11,30 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+def get_cache():
+    environment_ready = all(
+        os.environ.get(f'MEMCACHIER_{key}', False)
+        for key in ['SERVERS', 'USERNAME', 'PASSWORD']
+    )
+    if not environment_ready:
+        cache = {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
+    else:
+        servers = os.environ['MEMCACHIER_SERVERS']
+        username = os.environ['MEMCACHIER_USERNAME']
+        password = os.environ['MEMCACHIER_PASSWORD']
+        cache = {
+            'default': {
+                'BACKEND': 'django_bmemcached.memcached.BMemcached',
+                'TIMEOUT': None,
+                'LOCATION': servers,
+                'OPTIONS': {
+                    'username': username,
+                    'password': password,
+                }
+            }
+        }
+    return {'default': cache}
+
+
+CACHES = get_cache()
