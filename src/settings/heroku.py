4d297b86e1f8dbcbd5a14dacf113f849f21c9263
kobtsev-m <kobtsev.x@gmail.com>
2021-01-31 17:35:50 +0700

    Settings correction

--- src/settings/heroku.py
+++ src/settings/heroku.py
@@ -6,7 +6,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
