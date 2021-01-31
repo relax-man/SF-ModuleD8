953b6f8bbaada56f9c720b5e3e036cfa3fa2850c
kobtsev-m <kobtsev.x@gmail.com>
2021-01-31 22:08:45 +0700

    Django-heroku configure

--- src/settings/heroku.py
+++ src/settings/heroku.py
@@ -1,6 +1,7 @@
 from settings.base import *
 
 import dj_database_url
+import django_heroku
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -12,29 +13,16 @@ DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
-def get_cache():
-    environment_ready = all(
-        os.environ.get(f'MEMCACHIER_{key}', False)
-        for key in ['SERVERS', 'USERNAME', 'PASSWORD']
-    )
-    if not environment_ready:
-        cache = {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
-    else:
-        servers = os.environ['MEMCACHIER_SERVERS']
-        username = os.environ['MEMCACHIER_USERNAME']
-        password = os.environ['MEMCACHIER_PASSWORD']
-        cache = {
-            'default': {
-                'BACKEND': 'django_bmemcached.memcached.BMemcached',
-                'TIMEOUT': None,
-                'LOCATION': servers,
-                'OPTIONS': {
-                    'username': username,
-                    'password': password,
-                }
-            }
+django_heroku.settings(locals())
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_bmemcached.memcached.BMemcached',
+        'TIMEOUT': None,
+        'LOCATION': os.environ['MEMCACHIER_SERVERS'],
+        'OPTIONS': {
+            'username': os.environ['MEMCACHIER_USERNAME'],
+            'password': os.environ['MEMCACHIER_PASSWORD'],
         }
-    return {'default': cache}
-
-
-CACHES = get_cache()
+    }
+}
